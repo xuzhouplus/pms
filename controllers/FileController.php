84d74202387bbb388e40617e4af01b1de340046e
xuzhou <xuzhou@chinamcloud.com>
2020-11-06 18:52:28 +0800

    添加文件删除接口

--- controllers/FileController.php
+++ controllers/FileController.php
@@ -10,7 +10,10 @@ use yii\web\UploadedFile;
 class FileController extends RestController
 {
 
-	public function actionIndex()
+	public function actionIndex(){
+
+	}
+	public function actionUpload()
 	{
 		$file = new File();
 		$file->load(\Yii::$app->request->post(), '');
@@ -19,5 +22,15 @@ class FileController extends RestController
 		$data['url'] = $file->getUrl();
 		unset($data['path']);
 		return $this->response($data, 'Upload succeed');
+	}
+
+	public function actionDelete()
+	{
+		$file = File::find()->where(['id' => \Yii::$app->request->getBodyParam('id')])->limit(1)->one();
+		if ($file) {
+			$file->remove();
+			$file->delete();
+		}
+		return $this->response(null, 'Delete succeed');
 	}
 }
